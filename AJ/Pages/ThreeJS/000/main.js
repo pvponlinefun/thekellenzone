e0e1fffd54eee4e04b641a0832f9e9d82771fe67
Siffrin Greytail <siffringreytail@gmail.com>
2023-12-14 15:19:36 -0800

    Updated homepage

---
+++ AJ/Pages/ThreeJS/000/main.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three';
+
+class ThreeJSWorld
+{
+    constructor()
+    {
+        this._Initialize();
+    }
+
+    _Initialize() {
+        // this._threejs = new THREE.WebG
+    }
+}
